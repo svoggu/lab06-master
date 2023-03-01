ae08e3e62286f5e0f428196ea88997077a038b4e
svoggu <sirishavoggu@yahoo.co.in>
2023-02-28 20:36:06 -0800

    combine lab5 and lab6

---
+++ js/lab5.js
@@ -0,0 +1,19 @@
+let miles;
+let gallons;
+let mpg;
+let again = 'y';
+
+do {
+    miles = parseFloat(prompt('Enter miles driven'));
+    gallons = parseFloat(prompt('How many gallons does your car\'s tank hold'));
+    if (!isNaN(miles) && miles > 0 && !isNaN(gallons) && gallons > 0) {
+        mpg = miles / gallons;
+        console.log(`Your car gets ${mpg.toFixed(2)} miles per gallon.`);
+    } else {
+        alert('One or both entries are invalid.');
+    }
+    again = prompt('Run application again? (y or n)', 'y');
+    
+} while(again !=="n");
+
+console.log('Application has exited.');
